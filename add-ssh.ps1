083a09cf67379cbef2fee4656282938bded4e57c
NicklasBergkvist <33221125+NicklasBergkvist@users.noreply.github.com>
2023-11-01 12:59:34 +0100

    Update add-ssh.ps1

--- add-ssh.ps1
+++ add-ssh.ps1
@@ -20,7 +20,7 @@ $cert = "$tempfolder\cert.pem"
 & "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" rsa -in $priv -outform PEM -pubout -out $pub
 
 # Interact with the YubiKey
-& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -adelete-certificate -s9a
-& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -s 9a -a import-key -i $priv
+& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -adelete-certificate -s9a # -k "<management key>"
+& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -s 9a -a import-key -i $priv # -k "<management key>"
 & "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -a verify-pin -a selfsign-certificate --valid-days 3333 -s 9a -S "/CN=SSH key/" -i $pub -o $cert
-& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -a import-certificate -s 9a -i $cert
+& "C:\Program Files\Yubico\Yubico PIV Tool\bin\yubico-piv-tool.exe" -a import-certificate -s 9a -i $cert # -k "<management key>"
